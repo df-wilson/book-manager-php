e2e7f02ff9f7887ca62bf8cd756656de85f2064b
Dean Wilson <deanwilsonbc@gmail.com>
2019-12-19 17:35:43 -0800

    Create migration for the books table.

---
+++ database/migrations/2019_12_17_212321_create_books_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('title')->required();
+            $table->string('author')->required();
+            $table->integer('year')->nullable();
+            $table->boolean('read');
+            $table->integer('rating')->nullable();
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
