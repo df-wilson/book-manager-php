2e8e22535fdb2792d6d1df91bd2638b885165437
Dean Wilson <deanwilsonbc@gmail.com>
2019-12-19 17:31:06 -0800

    Add seeder file for the books table so that there is some data for testing.

---
+++ database/seeds/BooksTableSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class BooksTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('books')->insert([
+            "user_id" => 1,
+            "title" => 'The Institute',
+            "author" => 'Stephen King',
+            "year" => 2019,
+            "read" => true,
+            "rating" => 4,
+            "created_at" => new DateTime(),
+            "updated_at" => new DateTime(),
+        ]);
+
+        DB::table('books')->insert([
+            "user_id" => 1,
+            "title" => 'Blue Mars',
+            "author" => 'Kim Stanley Robinson',
+            "year" => 1996,
+            "read" => true,
+            "rating" => 4,
+            "created_at" => new DateTime(),
+            "updated_at" => new DateTime(),
+        ]);
+
+        DB::table('books')->insert([
+            "user_id" => 2,
+            "title" => 'Omega',
+            "author" => 'Jack McDevitt',
+            "year" => 2002,
+            "read" => true,
+            "rating" => 5,
+            "created_at" => new DateTime(),
+            "updated_at" => new DateTime(),
+        ]);
+
+
+    }
+}
