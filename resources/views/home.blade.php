a1fc9b4960be90a98e8a8685dee2e5d95906bd8e
Dean Wilson <deanwilsonbc@gmail.com>
2020-01-30 19:08:17 -0800

    Use common layout file.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <title>Book Manager</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" type="image/png" href="/img/favicon.png">
-    <link href="/css/bootstrap.min.css" type="text/css" rel="stylesheet" />
-    <link href="/css/site.css" type="text/css" rel="stylesheet" />
-</head>
-<body>
+@extends('layouts.app')
+
+@section('content')
 
 @guest
 <input type="hidden" id="isLoggedIn" name="isLoggedIn" value="false">
@@ -17,63 +9,7 @@
 @endif
 
 <div id="app">
-    <nav class="navbar navbar-expand-md navbar-light bg-light">
-        <span><router-link to="/" class="navbar-brand">Book Manager</router-link></span>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav">
-                @auth
-                <li class="nav-item">
-                    <span><router-link to="/add" class="nav-link" id="add-menu">Add</router-link></span>
-                </li>
-                <li class="nav-item">
-                    <span><router-link to="/book-manager" class="nav-link">Show All</router-link></span>
-                </li>
-                @endauth
-            </ul>
-        </div>
-        <ul class="navbar-nav ml-auto">
-            @guest
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-            </li>
-            @else
-            <li class="nav-item">
-                <a class="dropdown-item" href="{{ route('logout') }}"
-                   onclick="event.preventDefault();
-                   document.getElementById('logout-form').submit();">
-                    {{ __('Logout') }}
-                </a>
-
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                    @csrf
-                </form>
-            </li>
-            @endguest
-        </ul>
-    </nav>
-
     <router-view></router-view>
 </div>
 
-<script src="/js/vue.min.js"></script>
-<script src="/js/vue-router.min.js"></script>
-<script src="/js/jquery.js"></script>
-<script src="/js/popper.min.js"></script>
-<script src="/js/bootstrap.min.js"></script>
-<script src="/js/axios.min.js"></script>
-<script src="/js/rate.js"></script>
-<script src="/js/add-book.js"></script>
-<script src="/js/start.js"></script>
-<script src="/js/login.js"></script>
-<script src="/js/register.js"></script>
-<script src="/js/book-manager.js"></script>
-<script src="/js/app.js"></script>
-</body>
-</html>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,80 +1,81 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <title>{{ config('app.name', 'Book Manager') }}</title>
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>{{ config('app.name', 'Book Manager') }}</title>
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+        <link rel="shortcut icon" type="image/png" href="/img/favicon.png">
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+        <link href="/css/bootstrap.min.css" type="text/css" rel="stylesheet" />
+        <link href="/css/site.css" type="text/css" rel="stylesheet" />
+    </head>
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Book Manager') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+        <nav class="navbar navbar-expand-md navbar-light bg-light">
+            <span><router-link to="/" class="navbar-brand">Book Manager</router-link></span>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav">
+                    @auth
+                    <li class="nav-item">
+                        <span><router-link to="/add" class="nav-link" id="add-menu">Add</router-link></span>
+                    </li>
+                    <li class="nav-item">
+                        <span><router-link to="/book-manager" class="nav-link">Show All</router-link></span>
+                    </li>
+                    @endauth
+                </ul>
             </div>
-        </nav>
+            <ul class="navbar-nav ml-auto">
+                @guest
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                </li>
+                @else
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                           document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
 
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </li>
+                @endguest
+            </ul>
+        </nav>
         <main class="py-4">
             @yield('content')
         </main>
     </div>
+
+    <script src="/js/vue.min.js"></script>
+    <script src="/js/vue-router.min.js"></script>
+    <script src="/js/jquery.js"></script>
+    <script src="/js/popper.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+    <script src="/js/axios.min.js"></script>
+    <script src="/js/rate.js"></script>
+    <script src="/js/add-book.js"></script>
+    <script src="/js/start.js"></script>
+    <script src="/js/login.js"></script>
+    <script src="/js/register.js"></script>
+    <script src="/js/book-manager.js"></script>
+    <script src="/js/app.js"></script>
 </body>
 </html>
